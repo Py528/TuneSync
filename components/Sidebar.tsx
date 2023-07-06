be9b22038cecfa1fcc25873676ab6deb707951bc
Py528 <pranaav.shinde5280@gmail.com>
2023-07-06 19:24:55 +0530

    TuneSync App Struct

---
+++ components/Sidebar.tsx
@@ -0,0 +1,29 @@
+'use client';
+import React, { useMemo } from 'react';
+import { usePathname } from 'next/navigation';
+import { HiHome } from 'react-icons/hi';
+import { BiSearch } from 'react-icons/bi';
+interface SidebarProps {
+	children: React.ReactNode;
+}
+const Sidebar: React.FC<SidebarProps> = ({ children }) => {
+	const pathname = usePathname();
+	const routes = useMemo(
+		() => [
+			{ icon: HiHome, label: 'Home', active: pathname !== 'search', href: '/' },
+			{
+                icon: BiSearch,
+				label: 'Search',
+				active: pathname === 'search',
+				href: '/search',
+			},
+		],
+		[pathname]
+	);
+	return <div className='flex h-full'>
+        {/* {children} */}
+        Sidebar
+        </div>;
+};
+
+export default Sidebar;
